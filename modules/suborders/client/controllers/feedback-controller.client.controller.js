3210de86bd38f147d30cd6c14a7b6687962989cf
braisman <abcbrais@gmail.com>
2016-05-27 17:01:00 -0600

    Se agregaron los archivos nuevos del feedback

---
+++ modules/suborders/client/controllers/feedback-controller.client.controller.js
@@ -0,0 +1,16 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('suborders')
+    .controller('FeedbackController', FeedbackController);
+
+  FeedbackController.$inject = ['$scope', '$state', 'Authentication', 'suborderResolve'];
+
+  function FeedbackController($scope, $state, Authentication, suborder) {
+    var vm = this;
+
+    vm.authentication = Authentication;
+    vm.suborder = suborder;
+  }
+})();
